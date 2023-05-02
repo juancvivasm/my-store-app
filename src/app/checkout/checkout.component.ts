37a16f1411cc6cb2353b9cc093f8d1e3ff00aef1
JC <juanc.vivasm@gmail.com>
2023-05-02 17:31:54 -0400

    Improving validations

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -23,12 +23,42 @@ export class CheckoutComponent {
     }
   }
 
+  isValidCheckout(order: Cart): boolean {
+    const { fullName, address, creditCardNumber } = order
+
+    if (!fullName.trim()) {
+      alert('Full name must be defined')
+      return false
+    }
+
+    if (fullName.length < 3) {
+      alert('Full name must minimum 03 characters')
+      return false
+    }
+
+    if (!address.trim()) {
+      alert('Address must be defined')
+      return false
+    }
+
+    if (address.length < 6) {
+      alert('Address must minimum 03 characters')
+      return false
+    }
+
+    if (creditCardNumber.toString().length < 16) {
+      alert('Credit card number must be 16 digit number')
+      return false
+    }
+
+    return true
+  }
+
   submitForm(): void {
     //console.log(`Full name: ${this.order.fullName} - Address: ${this.order.address} - Credit card number: ${this.order.creditCardNumber}`)
-    //this.order.items = this.cartService.getCartDetails() 
-    //this.order.total = this.cartService.getTotal()
-    //console.log(this.order)
-    this.invoice.emit(this.order)
+    if (this.isValidCheckout(this.order)) {
+      this.invoice.emit(this.order)
+    }   
   }
 
 }
